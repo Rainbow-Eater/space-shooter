ac0b338be769e3212c4f5a9161e309d593f1c712
Rainbow-Eater <mdramadesu@gmail.com>
2024-08-29 02:18:55 +0300

    game/small code fix

--- src/game/scenes/game-scene.ts
+++ src/game/scenes/game-scene.ts
@@ -7,17 +7,16 @@ export class GameScene extends Phaser.Scene {
     super({ key: 'GameScene' })
   }
 
-  preload() {}
+  preload() { }
 
   create() {
     console.log('game scene')
     new Player(this)
     // new ScoutEnemy(this, this.scale.width / 2, 0)
     new FighterEnemy(this, this.scale.width / 2, 0)
-    const a = [1, 2, 3]
   }
 
-  update() {}
+  update() { }
 }
 
 export default GameScene
